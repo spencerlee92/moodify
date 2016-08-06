bd96bda0743fcfcf2655851b3e467de7bb4b114c
Spencer Lee <Spencer Lee>
2016-08-06 13:50:22 +1200

    This is the moodify website

---
+++ helloPerson.js
@@ -0,0 +1,6 @@
+function hello(person) {
+    return "Hello, " + person;
+}
+var user = "What is hello";
+hello(23823);
+//document.body.innerHTML = greeter(user);     
---
+++ helloPerson.ts
@@ -0,0 +1,9 @@
+function hello(person: any) {
+    return "Hello, " + person;
+}
+
+var user = "What is hello";
+
+
+hello(23823);
+//document.body.innerHTML = greeter(user);     
